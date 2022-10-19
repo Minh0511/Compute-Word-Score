6ec0637e21d2854377fe558addcf380cf57e6068
Minh Phạm Vũ <minh.pv@teko.vn>
2022-10-19 19:44:45 +0700

    Add C2 control flow tests

--- main_test.go
+++ main_test.go
@@ -5,7 +5,67 @@ import (
 	"testing"
 )
 
-func TestComputeWordScore(t *testing.T) {
-	expected := computeWordScore(11, 7, 10)
+func TestComputeWordScoreLine2And3True(t *testing.T) {
+	expected := computeWordScore(-1, 0, 0)
 	assert.Equal(t, "Invalid input", expected)
+}
+
+func TestComputeWordScoreLine2And3False(t *testing.T) {
+	expected := computeWordScore(1, 1, 1)
+	assert.Equal(t, "F", expected)
+}
+
+func TestComputeWordScoreLine7False(t *testing.T) {
+	expected := computeWordScore(1, 1, 8)
+	assert.Equal(t, "F", expected)
+}
+
+func TestComputeWordScoreLine12True(t *testing.T) {
+	expected := computeWordScore(1, 1, 10)
+	assert.Equal(t, "F", expected)
+}
+
+func TestComputeWordScoreLine12False(t *testing.T) {
+	expected := computeWordScore(9, 9, 9)
+	assert.Equal(t, "A+", expected)
+}
+
+func TestComputeWordScoreLine14True(t *testing.T) {
+	expected := computeWordScore(1, 6, 10)
+	assert.Equal(t, "D", expected)
+}
+
+func TestComputeWordScoreLine16True(t *testing.T) {
+	expected := computeWordScore(2, 6, 10)
+	assert.Equal(t, "D+", expected)
+}
+
+func TestComputeWordScoreLine18True(t *testing.T) {
+	expected := computeWordScore(1, 7, 10)
+	assert.Equal(t, "C", expected)
+}
+
+func TestComputeWordScoreLine20True(t *testing.T) {
+	expected := computeWordScore(1, 9, 10)
+	assert.Equal(t, "C+", expected)
+}
+
+func TestComputeWordScoreLine22True(t *testing.T) {
+	expected := computeWordScore(1, 10, 10)
+	assert.Equal(t, "B", expected)
+}
+
+func TestComputeWordScoreLine24True(t *testing.T) {
+	expected := computeWordScore(4, 10, 10)
+	assert.Equal(t, "B+", expected)
+}
+
+func TestComputeWordScoreLine26True(t *testing.T) {
+	expected := computeWordScore(5, 10, 10)
+	assert.Equal(t, "A", expected)
+}
+
+func TestComputeWordScoreLine30True(t *testing.T) {
+	expected := computeWordScore(9, 9, 9)
+	assert.Equal(t, "A+", expected)
 }
